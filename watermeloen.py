d02f72cf2dee509fd93761aba2eafb81d929fa37
liekenijhuis <147824917+liekenijhuis@users.noreply.github.com>
2025-11-06 14:38:16 +0100

    Update watermeloen.py

--- watermeloen.py
+++ watermeloen.py
@@ -6,7 +6,7 @@
 # ------------------------------------------------------
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 import numpy as np
 import folium
 import requests
