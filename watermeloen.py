92afa2620d090b6389da3d6c0e338250717077b8
liekenijhuis <147824917+liekenijhuis@users.noreply.github.com>
2025-11-06 15:29:10 +0100

    Update watermeloen.py

--- watermeloen.py
+++ watermeloen.py
@@ -1,9 +1,10 @@
-
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 from statsmodels.tsa.statespace.varmax import VARMAX
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-import numpy as np
-import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 # ------------------- Pagina 3 --------------------------
 #elif page == "📊 Voorspellend model":
